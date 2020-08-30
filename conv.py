31ff7f846a909bdc4a33cd61ea2eb906ba468b45
syoch <hacksyoch@gmail.com>
2020-08-30 21:08:13 +0900

    コンバート開始関数の定義

---
+++ conv.py
@@ -0,0 +1,32 @@
+import ast
+
+def eval_sentence(sentence:ast.stmt):
+    print(sentence.__class__,sentence.__dict__,"\n") # debug code
+
+def conv(filename:str,dest:str):
+    """
+    Convert python to c++ source code
+
+    Parameter
+
+       Name  | Description|Type
+    ---------+------------+-----
+    filename |   source   | str
+    dest     |   dest     | str
+
+    Returns:None
+    """
+    fp=open(filename,"r")
+    data=fp.read()
+    fp.close()
+
+    src=ast.parse(data,filename)
+
+    for sentence in src.body:
+        eval_sentence(sentence)
+
+# +-----------------------+
+# |          Test         |
+# +-----------------------+
+if __name__ == "__main__":
+    conv("conv.py","dest.cpp")
