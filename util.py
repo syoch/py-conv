bd97d5999a773d1d8f03dee60d3b9acd41413daf
syoch <hacksyoch@gmail.com>
2020-08-31 08:36:48 +0900

    モード指定可能に

--- util.py
+++ util.py
@@ -1,18 +1,39 @@
 from datamgr import get
 import sentences,exprs,basetype
+import enum
 
-def conv(raw):
+class modes(enum.Enum):
+    EXPR=0
+    SENT=1
+    BOTH=2
+
+def conv(raw,mode:modes=modes.BOTH):
     typename=raw.__class__.__name__
-    if typename in sentences.table:
-        return sentences.table[typename](raw)
-    elif typename in exprs.table:
-        return exprs.table[typename](raw)
-    elif typename in basetype.table:
-        return basetype.table[typename](raw)
-    else:
-        print("Unknown Type "+typename)
-        print(get("srcs"))
-        exit()
+    if mode==modes.EXPR:
+        if typename in exprs.table:
+            return exprs.table[typename](raw)
+        else:
+            print("Unknown Expr Type "+typename)
+            print(get("srcs"))
+            exit()
+    elif mode==modes.SENT:
+        if typename in sentences.table:
+            return sentences.table[typename](raw)
+        else:
+            print("Unknown Sentence Type "+typename)
+            print(get("srcs"))
+            exit()
+    elif mode==modes.BOTH:
+        if typename in sentences.table:
+            return sentences.table[typename](raw)
+        elif typename in exprs.table:
+            return exprs.table[typename](raw)
+        elif typename in basetype.table:
+            return basetype.table[typename](raw)
+        else:
+            print("Unknown Type "+typename)
+            print(get("srcs"))
+            exit()
 
 def walk_shallow(obj,f=""):
     tmp=""
