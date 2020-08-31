8aa7a27dfad32f76d045da9e6321de22e9c622af
syoch <hacksyoch@gmail.com>
2020-08-31 09:09:44 +0900

    自明なモード指定

--- sentences.py
+++ sentences.py
@@ -1,7 +1,7 @@
 #Defines analyze of ast sentence functions
 
 import ast
-from exprs import expr_call
+from exprs import expr_args, expr_call
 import os
 import util
 import datamgr
@@ -19,17 +19,17 @@ def sent_import(sentence:ast.Import,f=""):
 
 def sent_funcdef(sentence:ast.FunctionDef,f=""):
     return \
-        f+"Any "+sentence.name+"("+util.conv(sentence.args)+")"+"{\n"+\
+        f+"Any "+sentence.name+"("+expr_args(sentence.args)+")"+"{\n"+\
             util.walk_shallow(sentence.body,f+"  ")+\
         "}\n"
 
 def sent_ret(sentence:ast.Return,f=""):
-    return f+"Return "+util.conv(sentence.value)+";\n"
+    return f+"Return "+util.conv(sentence.value,util.modes.EXPR)+";\n"
 
 def sent_assign(sentence:ast.Assign,f=""):
     ret=""
     tmp=sentence.targets
-    tmp=[util.conv(target) for target in tmp]
+    tmp=[util.conv(target,util.modes.EXPR) for target in tmp]
     tmp=", ".join(tmp)
     ret+=tmp
     ret+=" = "
@@ -38,15 +38,23 @@ def sent_assign(sentence:ast.Assign,f=""):
 
 def sent_for(sentence:ast.For,f=""):
     return \
+        f+"for ("+util.conv(sentence.target,util.modes.EXPR)+" in "+util.conv(sentence.iter,util.modes.EXPR)+"){\n"+\
+            util.walk_shallow(sentence.body,f+"  ")+\
+        f+"}\n"
+def sent_if(sentence:ast.If,f=""):
+    print(util.conv(sentence.test))
+    print(sentence.__dict__)
+    exit()
+    return \
         f+"for ("+util.conv(sentence.target)+" in "+util.conv(sentence.iter)+"){\n"+\
             util.walk_shallow(sentence.body,f+"  ")+\
         f+"}\n"
-
 table={
     "Import":sent_import,
     "FunctionDef":sent_funcdef,
     "Return":sent_ret,
     "Assign":sent_assign,
     "Call":lambda val,f="":f+expr_call(val)+";\n",
-    "For":sent_for
+    "For":sent_for,
+    "If":sent_if
 }
