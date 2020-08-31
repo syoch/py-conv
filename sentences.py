2d27f9783c8fec984999aaa79b2d6125e9f56c0e
syoch <hacksyoch@gmail.com>
2020-08-31 19:26:05 +0900

    Revert "funcdefの簡易関数プロトタイプの実装"
    This reverts commit 07f8ca2a72cbf0afc3fc850abeda7a2060fd9366, reversing
    changes made to 8259df79b50f53afcb4666c7f38eab29539eca73.

---
+++ sentences.py
@@ -0,0 +1,26 @@
+#Defines analyze of ast sentence functions
+
+import ast
+import os
+import util
+import datamgr
+
+def sent_improt(sentence:ast.Import):
+    tmp=""
+    for a in sentence.names:
+        name=util.conv(a)
+        if os.path.exists(name+".py"):
+            tmp+="#include \""+name+".cpp"+"\"\n"
+            datamgr.push("srcs",name+".py")
+        else:
+            tmp+="#include <"+name+">\n"
+    return tmp
+
+def sent_funcdef(sentence:ast.FunctionDef):
+    return "Any "+sentence.name+"("+util.conv(sentence.args)+")"+"\n"
+    #print(sentence.body)
+
+table={
+    "Import":sent_improt,
+    "FunctionDef":sent_funcdef
+}
