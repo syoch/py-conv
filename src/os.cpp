ef50e060c9117577e6cd6e298457b16644cc2eb9
syoch <hacksyoch@gmail.com>
2020-09-05 16:11:28 +0900

    os依存関数の分離

--- src/os.cpp
+++ src/os.cpp
@@ -1,5 +1,5 @@
-#include <direct.h>
-#include <sys/stat.h>
+#include "../lib/platform/file"
+
 #include <stdio.h>
 #include <iostream>
 #include "../lib/os"
@@ -10,9 +10,9 @@ void os::mkdir(Any name, bool exist_ok = false)
 	struct stat statBuf;
 	int result = 1;
 	if (stat(dir, &statBuf) != 0) {
-		result = _mkdir(dir);
+		result = platform::mkdir(dir);
 	} else if (exist_ok == true && stat(dir, &statBuf) == 0) {
-		result = _mkdir(dir);
+		result = platform::mkdir(dir);
 	}
 	if (result == 1) {
 		throw OSError("Cannot rely on checking for EEXIST, since the operating system could give priority to other errors like EACCES or EROFS");
@@ -21,7 +21,7 @@ void os::mkdir(Any name, bool exist_ok = false)
 
 void os::rmdir(Any name)
 {
-	int result = _rmdir(std::any_cast<const char*>(name));
+	int result = platform::rmdir(name);
 	if (result == 1) {
 		throw;
 	}
@@ -30,11 +30,10 @@ void os::rmdir(Any name)
 void os::renames(Any old, Any _new)
 {
 	os os;
-	struct stat statBuf;
-	if (stat(std::any_cast<const char*>(old), &statBuf) != 0) {
+	if (!platform::exist(old)) {
 		os.mkdir(_new);
 	} else {
-		if (rename(std::any_cast<const char*>(old), std::any_cast<const char*>(_new)) != 0) {
+		if (platform::rename(old,_new) != 0) {
 			return;
 		}
 	}
