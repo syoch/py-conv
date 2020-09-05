8dd5e9f79f945f6687f4dfb2343e0d4235be2ad2
syoch <hacksyoch@gmail.com>
2020-09-06 07:19:30 +0900

    キューをdequeに変更

--- datamgr.py
+++ datamgr.py
@@ -1,53 +1,6 @@
 from typing import Any
 import collections
 
-#Queue
-
-queue={}
-
-def create(name:str) -> None:
-    if name in queue:
-        raise f"queue `{name}` is already defined"
-    queue[name]=collections.deque()
-
-def remove(name:str) -> None:
-    if name not in queue:
-        raise f"queue `{name}` isn't defined"
-    del queue[name]
-
-def get(name:str) ->None:
-    if name not in queue:
-        raise f"queue `{name}` isn't defined"
-    return queue[name]
-
-def push(name:str,data:Any) -> None:
-    if not name in queue:
-        create(name)
-    queue[name].append(data)
-
-def pop(name:str) -> None:
-    if not name in queue:
-        raise f"queue `{name}` isn't defined"
-    return queue[name].pop()
-
-def pushleft(name:str,data:Any) -> None:
-    if not name in queue:
-        create(name)
-    queue[name].appendleft(data)
-
-def popleft(name:str) -> None:
-    if not name in queue:
-        raise f"queue `{name}` isn't defined"
-    return queue[name].popleft()
-
-def have_data(name:str) -> bool:
-    if not name in queue:
-        return False
-    if len(queue[name])==0:
-        return False
-    else:
-        return True
-
 # Value
 values={}
 def set_val(name:str,value:Any)->None:
@@ -86,3 +39,31 @@ def set_dict(name:str,key:str,value:Any) ->None:
     if name not in dict_table:
         raise f"dict `{name}` isn't defined"
     dict_table[name][key]=value
+
+# queue
+from collections import deque
+class queue: # namespace
+    table={}
+    @staticmethod
+    def create(name:str)->None:
+        if name in queue.table:
+            raise Exception(f"{name} is already defined")
+        queue.table[name]=deque()
+
+    @staticmethod
+    def put(name:str,val:Any)->None:
+        if name not in queue.table:
+            raise Exception(f"{name} is not already defined")
+        queue.table[name].append(val)
+
+    @staticmethod
+    def get(name:str)->Any:
+        if name not in queue.table:
+            raise Exception(f"{name} is not already defined")
+        return queue.table[name].popleft()
+    
+    @staticmethod
+    def empty(name:str) -> Any:
+        if name not in queue.table:
+            raise Exception(f"{name} is not already defined")
+        return len(queue.table[name])==0
