a20f5c645e2e89d5950ab6c7d385098baa208637
Yuki <32415464+Yukikamome316@users.noreply.github.com>
2020-09-06 01:10:43 +0900

    isdir/isfile関数の実装

--- src/genericpath.cpp
+++ src/genericpath.cpp
@@ -1,6 +1,6 @@
 #include "../lib/genericpath"
 #include <sys/stat.h>
-#include<iostream>
+#include <iostream>
 
 bool path::exists(Any path)
 {
@@ -8,6 +8,34 @@ bool path::exists(Any path)
     const char* file = std::any_cast<const char*>(path);
     int ret = stat(file, &st);
     if (ret == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool path::isdir(Any path)
+{
+    struct stat st;
+    int result;
+    result = stat(std::any_cast<const char*>(path), &st);
+    if (result != 0) {
+        return false;
+    } else if ((st.st_mode & S_IFMT) == S_IFDIR) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool path::isfile(Any path)
+{
+    struct stat st;
+    int result;
+    result = stat(std::any_cast<const char*>(path), &st);
+    if (result != 0) {
+        return false;
+    } else if ((st.st_mode & S_IFMT) != S_IFDIR) {
         return true;
     } else {
         return false;
