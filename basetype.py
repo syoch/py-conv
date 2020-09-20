4a62d9c5293634b53fc48da6b2e1abf138ce8cb5
syoch <hacksyoch@gmail.com>
2020-09-21 07:16:45 +0900

    bytesの予期しない文字列のサポート

--- basetype.py
+++ basetype.py
@@ -1,9 +1,14 @@
+def bytes2str(b):
+    s=""
+    for a in b:
+        s+=chr(a)
+    return s
 table={
     "NoneType":lambda a:"nullptr",
     "str":lambda a:"\""+a.replace("\\","\\\\").replace("\n","\\n").replace("\"","\\\"")+"\"",
     "int":lambda a:str(a),
     "bool":lambda a:"bool",
-    "bytes":lambda a:a.decode(),
+    "bytes":bytes2str,
     "float":lambda a:str(a),
     "ellipsis":lambda a:""
 }
