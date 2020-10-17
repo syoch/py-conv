8ff110bc21ee1f932554fc3d01c0632f5f683373
syoch <hacksyoch@gmail.com>
2020-10-18 01:36:45 +0900

    変換関数の定義

--- libnames.py
+++ libnames.py
@@ -29,3 +29,12 @@ names = [
     "uuid",             "warnings",        "wave",        "weakref",          "webbrowser",      "xdrlib",
     "zipapp",           "zipfile",         "zipimport"
 ]
+table={
+    "_abc":"_py_abc.cpp",
+    "_weakref":"weakref.cpp"
+}
+def fix(name):
+    global table
+    if name in table:
+        return table[name]
+    return name
